3a9db20c8f8373ccfd28b67aded3bed290de5cb8
Rogerio Pereira <hi@rogeriopereira.dev>
2021-07-21 21:55:22 -0400

    Aula: 02 - Listar Avaliações API Laravel

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\EvaluationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(){
     return response()->json(['message' => 'success']);
 });
+
+Route::get('/evaluations/{company}', [EvaluationController::class, 'index']);
